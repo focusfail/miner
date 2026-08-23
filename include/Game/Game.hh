ec7f970e26ace98d111d6a193673855e792c65d7
focusfail <pahu006@pm.me>
2026-08-23 11:46:51 +0200

    add player reach

--- include/Game/Game.hh
+++ include/Game/Game.hh
@@ -3,7 +3,6 @@
 #include "Player/Player.hh"
 #include "Render/BlockOverlayRenderer.hh"
 #include "Render/Debug/DebugRenderer.hh"
-#include "Render/TextureArray.hh"
 #include "TaskScheduler.h"
 
 #include "Platform/Window.hh"
@@ -17,13 +16,14 @@ struct DebugState {
 };
 
 struct GameState {
-    int blockBreakStagesPerMine = 1;
-    float blockMineTimeout = 0.1f;
+    int blockBreakStagesPerMine = 4;
+    float blockMineTimeout = 0.05f;
     float blockMineTimer = 0.0f;
+    float reach = 4.0f;
 };
 
 class Game : private NonCopyable, private NonMovable {
-  public:
+public:
     static Game &GetInstance() {
         static Game instance;
         return instance;
@@ -36,7 +36,7 @@ class Game : private NonCopyable, private NonMovable {
     Window &GetGameWindow() { return m_Window; };
     DebugState &GetDebugState() { return m_DebugState; }
 
-  private:
+private:
     Window m_Window;
     World m_World;
     Player m_Player;
