70e5b045befc12d6ad9ca45607439e2a49be673f
focusfail <pahu006@pm.me>
2026-08-23 11:47:41 +0200

    move impl to header

--- include/Render/GpuPool.hh
+++ include/Render/GpuPool.hh
@@ -3,7 +3,10 @@
 #include "Render/GpuBuffer.hh"
 #include "Util/BucketPool.hh"
 
+#include <cassert>
+#include <optional>
 #include <span>
+#include <vector>
 
 template <typename T> class GpuPool {
 public:
@@ -13,14 +16,37 @@ public:
     void Init(std::vector<Bucket> buckets);
     void Bind(uint32_t loc = 0);
 
-    auto Allocate(uint32_t count);
+    std::optional<Slot> Allocate(uint32_t count);
     void Upload(Slot slot, std::span<const T> data);
     void Free(Slot slot);
 
     uint32_t Handle() const { return m_Buffer.Handle(); }
 
 private:
-    T m_Type;
     GpuBuffer m_Buffer;
     BucketPool m_Pool;
 };
+
+template <typename T> void GpuPool<T>::Init(std::vector<Bucket> buckets) {
+    m_Pool.Init(std::move(buckets));
+
+    m_Buffer.Init();
+    m_Buffer.Allocate(m_Pool.Capacity());
+}
+
+template <typename T> void GpuPool<T>::Bind(uint32_t loc) {
+    m_Buffer.Bind(loc);
+}
+template <typename T>
+std::optional<typename GpuPool<T>::Slot> GpuPool<T>::Allocate(uint32_t count) {
+    return m_Pool.Allocate(sizeof(T) * count);
+}
+
+template <typename T>
+void GpuPool<T>::Upload(Slot slot, std::span<const T> data) {
+    assert(slot.size >= data.size_bytes());
+
+    m_Buffer.Upload(slot.offset, data.size_bytes(), data.data());
+}
+
+template <typename T> void GpuPool<T>::Free(Slot slot) { m_Pool.Free(slot); }
